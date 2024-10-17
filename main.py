cac4035d49ff77f8ad6aaf530270e8a7a16681f2
imoelle@work <moeller.ingo@gmx.de>
2024-10-17 13:58:00 +0200

    Animation Test with SpriteClass from Pygame

--- main.py
+++ main.py
@@ -14,10 +14,29 @@ import pygame
 
 from config import *
 
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 1
+        print(self.rect.left)
+        if self.rect.left > WINDOW_WIDTH:
+            self.rect.left = 50
+
+    def draw(self, scr):
+        screen.blit(self.image, self.rect)
+
+
 pygame.init()
 
 # Set up the drawing window
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+ball = Ball()
 
 # Run until the user asks to quit
 running = True
@@ -33,7 +52,11 @@ while running:
     screen.fill((255, 255, 255))
 
     # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    ball.draw(screen)
+    ball.update()
+
 
     # Flip the display
     pygame.display.flip()
+
+pygame.quit()
